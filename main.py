79e42f0873c3eea1f3182b9408c7b33d4a7a0279
LEO-LEAL <leoleal97@gmail.com>
2015-07-28 16:17:17 -0700

    cleaner map

--- main.py
+++ main.py
@@ -24,32 +24,10 @@ import urllib2
 import json
 from google.appengine.ext import ndb
 
-
-
-class Location(ndb.Model):
-    latitude = ndb.FloatProperty()
-    longitude = ndb.FloatProperty()
-    created = ndb.DateTimeProperty()
-
-class EventfulHandler(webapp2.RequestHandler):
-    def get(self):
-        url = "http://api.eventful.com/json/events/search?app_key=TpFKjZjQc76tZrpF&where=32.746682,-117.162741&within=25"
-        try:
-            result= urllib2.urlopen(url)
-            print(result)
-        except urllib2.URLError, e:
-            handleError(e)
-        #r=urllib2.urlopen("http://api.eventful.com/json/events/search?app_key=TpFKjZjQc76tZrpF&where=32.746682,-117.162741&within=25")
-        #s= r.read()
-        #d= json.loads(s)
-        #for x in d["events"]["event"]:
-        #    print x["title"].encode("utf-8")
-
-
 class LoginHanlder(webapp2.RequestHandler):
     def get(self):
             greeting = ('<a href="%s">Sign in with Google</a>' %
-                users.create_login_url('/location'))
+                users.create_login_url('/main'))
             template_vars = { 'greeting' :  greeting }
             template = jinja2_environment.get_template('templates/triptrap.html')
             self.response.write(template.render(template_vars))
@@ -58,30 +36,11 @@ class LoginHanlder(webapp2.RequestHandler):
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        query = Location.query()
-        data = query.fetch()
-
-class LocationHandler(webapp2.RequestHandler):
-    def get(self):
         user = users.get_current_user()
         greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
             (user.nickname(), users.create_logout_url('/')))
         self.response.write('<html><body>%s</body></html>' % greeting)
         template = jinja2_environment.get_template("templates/places.html")
-        lat = self.request.get('lat')
-        lon = self.request.get('lon')
-        url = ('http://api.openweathermap.org/data/2.5/weather?'
-        'lat=%s&lon=%s&units=Imperial&APPID=883c191fd8d3d4a18ed700f5f65dcfd4' % (lat, lon))
-        string = urllib2.urlopen(url).read()
-        dictionary = json.loads(string)
-        # logging.info(dictionary)
-        if lat == "" or lon == "":
-            form = True
-        else:
-            form = False
-            loc = Location(latitude=float(lat), longitude=float(lon),
-                created=datetime.datetime.now())
-            loc.put()
         self.response.write(template.render())
 
 jinja2_environment = jinja2.Environment(loader=
@@ -89,7 +48,5 @@ jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 app = webapp2.WSGIApplication([
     ('/', LoginHanlder),
-    ('/main', MainHandler),
-    ('/eventful', EventfulHandler)
-    ('/location', LocationHandler)
+    ('/main', MainHandler)
 ], debug=True)
