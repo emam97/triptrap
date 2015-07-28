6fd41f5d6baac0c312abd3a1bcbffb271bf833a3
LEO-LEAL <leoleal97@gmail.com>
2015-07-28 09:58:57 -0700

    login map merge

--- resources/places.js
+++ resources/places.js
@@ -1,35 +1,3 @@
-<html>
-  <head>
-    <title>Place searches</title>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
-    <style>
-      html, body, #map-canvas {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-
-    </style>
-
-<script>
-var lat;
-var lon;
-window.onload = function() {
-  var startPos;
-  var geoSuccess = function(position) {
-    startPos = position;
-    lat = startPos.coords.latitude;
-    lon = startPos.coords.longitude;
-    initialize();
-  };
-  navigator.geolocation.getCurrentPosition(geoSuccess);
-};
-</script>
-
-
-    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places"></script>
-    <script>
 var map;
 var infowindow;
 
@@ -71,12 +39,3 @@ function createMarker(place) {
     infowindow.open(map, this);
   });
 }
-
-// google.maps.event.addDomListener(window, 'load', initialize);
-
-    </script>
-  </head>
-  <body>
-    <div id="map-canvas"></div>
-  </body>
-</html>
