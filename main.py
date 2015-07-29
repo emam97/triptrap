5d4b90eb0b0099b39480fac229f637761a68b554
mikaela410 <mikaela.hunter41@gmail.com>
2015-07-28 17:00:52 -0700

    corrections for eventful

--- main.py
+++ main.py
@@ -23,8 +23,93 @@ import logging
 import urllib2
 import json
 from google.appengine.ext import ndb
+import argparse
+import pprint
+import sys
+import urllib
+import oauth2
+
+API_HOST = 'api.yelp.com'
+DEFAULT_TERM = 'dinner'
+DEFAULT_LOCATION = 'Atlanta, GA'
+SEARCH_LIMIT = 4
+SEARCH_PATH = '/v2/search/'
+BUSINESS_PATH = '/v2/business/'
+
+# OAuth credential placeholders that must be filled in by users.
+CONSUMER_KEY = 'F6R9r8flzEZJZZxOd-KpMg'
+CONSUMER_SECRET = '0qZ2wcKfMG56pGSyJmLatyz81XY'
+TOKEN = 't-la5t5chvatXTSsDh7bHxzFMhfT92Gl'
+TOKEN_SECRET = 'f3fipu1Ym_YulH3n2dQ53HHoG3E'
+
+def request(host, path, url_params=None):
+    """Prepares OAuth authentication and sends the request to the API.
+
+    Args:
+        host (str): The domain host of the API.
+        path (str): The path of the API after the domain.
+        url_params (dict): An optional set of query parameters in the request.
+
+    Returns:
+        dict: The JSON response from the request.
+
+    Raises:
+        urllib2.HTTPError: An error occurs from the HTTP request.
+    """
+    url_params = url_params or {}
+    url = 'http://{0}{1}?'.format(host, urllib.quote(path.encode('utf8')))
+
+    consumer = oauth2.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
+    oauth_request = oauth2.Request(method="GET", url=url, parameters=url_params)
+
+    oauth_request.update(
+        {
+            'oauth_nonce': oauth2.generate_nonce(),
+            'oauth_timestamp': oauth2.generate_timestamp(),
+            'oauth_token': TOKEN,
+            'oauth_consumer_key': CONSUMER_KEY
+        }
+    )
+    token = oauth2.Token(TOKEN, TOKEN_SECRET)
+    oauth_request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
+    signed_url = oauth_request.to_url()
 
 
+    conn = urllib2.urlopen(signed_url, None)
+    try:
+        response = json.loads(conn.read())
+    finally:
+        conn.close()
+    return response
+def search(term, location):
+    """Query the Search API by a search term and location.
+    Args:
+        term (str): The search term passed to the API.
+        location (str): The search location passed to the API.
+    Returns:
+        dict: The JSON response from the request.
+    """
+
+    url_params = {
+        'term': term.replace(' ', '+'),
+        'location': location.replace(' ', '+'),
+        'limit': SEARCH_LIMIT
+    }
+    return request(API_HOST, SEARCH_PATH, url_params=url_params)
+
+class YelpHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja2_environment.get_template('templates/yelp.html')
+        response = search('diner', 'Atlanta')
+        result = response['businesses'][0]
+        name = result['name']
+        url = result['url']
+        address = result['location']
+        categories = result['categories']
+        template_var = { 'value1' : name, 'value2' : url,
+        'value3' : address, 'value4' : categories
+        }
+        self.response.write(template.render(template_var))
 
 class Location(ndb.Model):
     latitude = ndb.FloatProperty()
@@ -33,13 +118,25 @@ class Location(ndb.Model):
 
 class EventfulHandler(webapp2.RequestHandler):
     def get(self):
-        url = "http://api.eventful.com/json/events/search?app_key=TpFKjZjQc76tZrpF&where=47.61,-122.33&within=25"
+        template = jinja2_environment.get_template('templates/eventful.html')
+        lat = self.request.get('lat')
+        lon = self.request.get('lon')
+        url = ('http://api.eventful.com/json/events/search?app_key=TpFKjZjQc76tZrpF&where=%s,%s&within=25' % (lat,lon))
         r=urllib2.urlopen( url )
         s= r.read()
         d= json.loads(s)
+        logging.info(lat)
+        logging.info(lon)
+        if lat == "" or lon == "":
+            form = True
+        else:
+            form = False
+            loc = Location(latitude = float(lat), longitude=float(lon),
+            created=datetime.datetime.now())
+            loc.put()
         for x in d["events"]["event"]:
             self.response.write(x["title"].encode("utf-8"))
-            self.response.write(" ")
+            self.response.write(", ")
             self.response.write(x["venue_address"])
             self.response.write(", ")
             self.response.write(x["city_name"])
@@ -48,7 +145,8 @@ class EventfulHandler(webapp2.RequestHandler):
             self.response.write(", ")
             self.response.write(x["country_name"])
             self.response.write('</br>')
-            
+        self.response.write(template.render())
+
 class LoginHanlder(webapp2.RequestHandler):
     def get(self):
             greeting = ('<a href="%s">Sign in with Google</a>' %
@@ -94,5 +192,6 @@ app = webapp2.WSGIApplication([
     ('/', LoginHanlder),
     ('/main', MainHandler),
     ('/eventful', EventfulHandler),
-    ('/location', LocationHandler)
+    ('/location', LocationHandler),
+    ('/yelp', YelpHandler)
 ], debug=True)
